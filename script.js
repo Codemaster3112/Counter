6d1916ed0fcf1839f269f643e477dcebc8b744b6
Codemaster3112 <keyurmistry01@gmail.com>
2023-10-01 19:03:41 +0530

    Initial Commit

---
+++ script.js
@@ -0,0 +1,9 @@
+var score = 0;
+function increase(){
+    score++;
+    document.getElementById("count").innerHTML = score;
+}
+function decrease(){
+    score--;
+    document.getElementById("count").innerHTML = score;
+}
